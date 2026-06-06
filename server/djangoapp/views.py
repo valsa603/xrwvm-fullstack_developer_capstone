aadd68959552cd222dac328281d5d40a77d5befb
valsa603 <valus603@gmail.com>
2026-06-06 08:07:10 -0400

    Completed sentiment analysis microservice integration

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -7,6 +7,8 @@
 # from django.contrib.auth import logout
 # from django.contrib import messages
 # from datetime import datetime
+from .restapis import get_request
+from .restapis import analyze_review_sentiments, post_review
 from .models import CarMake, CarModel
 from django.http import JsonResponse
 from django.contrib.auth import login, authenticate
@@ -58,6 +60,54 @@ def get_cars(request):
 # @csrf_exempt
 # def registration(request):
 # ...
+#Update the `get_dealerships` render list of dealerships all by default, particular state if state is passed
+def get_dealerships(request, state="All"):
+    if(state == "All"):
+        endpoint = "/fetchDealers"
+    else:
+        endpoint = "/fetchDealers/"+state
+    dealerships = get_request(endpoint)
+    return JsonResponse({"status":200,"dealers":dealerships})
+
+def get_dealer_details(request, dealer_id):
+    if(dealer_id):
+        endpoint = "/fetchDealer/" + str(dealer_id)
+        dealership = get_request(endpoint)
+
+        return JsonResponse({
+            "status": 200,
+            "dealer": dealership
+        })
+    else:
+        return JsonResponse({
+            "status": 400,
+            "message": "Bad Request"
+        })
+
+def get_dealer_reviews(request, dealer_id):
+    # if dealer id has been provided
+    if(dealer_id):
+        endpoint = "/fetchReviews/dealer/"+str(dealer_id)
+        reviews = get_request(endpoint)
+        for review_detail in reviews:
+            response = analyze_review_sentiments(review_detail['review'])
+            print(response)
+            review_detail['sentiment'] = response['sentiment']
+        return JsonResponse({"status":200,"reviews":reviews})
+    else:
+        return JsonResponse({"status":400,"message":"Bad Request"})
+
+
+def add_review(request):
+    if(request.user.is_anonymous == False):
+        data = json.loads(request.body)
+        try:
+            response = post_review(data)
+            return JsonResponse({"status":200})
+        except:
+            return JsonResponse({"status":401,"message":"Error in posting review"})
+    else:
+        return JsonResponse({"status":403,"message":"Unauthorized"})
 
 # # Update the `get_dealerships` view to render the index page with
 # a list of dealerships
