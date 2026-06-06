2976d152088c52a7975c2f7b0ba6b5cc17d8df65
valsa603 <valus603@gmail.com>
2026-06-06 06:16:37 -0400

    Completed car model and get_cars implementation

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -7,13 +7,13 @@
 # from django.contrib.auth import logout
 # from django.contrib import messages
 # from datetime import datetime
-
+from .models import CarMake, CarModel
 from django.http import JsonResponse
 from django.contrib.auth import login, authenticate
 import logging
 import json
 from django.views.decorators.csrf import csrf_exempt
-# from .populate import initiate
+from .populate import initiate
 
 
 # Get an instance of a logger
@@ -37,6 +37,18 @@ def login_user(request):
         login(request, user)
         data = {"userName": username, "status": "Authenticated"}
     return JsonResponse(data)
+
+def get_cars(request):
+    count = CarMake.objects.filter().count()
+    print(count)
+    if(count == 0):
+        initiate()
+    car_models = CarModel.objects.select_related('car_make')
+    cars = []
+    for car_model in car_models:
+        cars.append({"CarModel": car_model.name, "CarMake": car_model.car_make.name})
+    return JsonResponse({"CarModels":cars})
+ 
 
 # Create a `logout_request` view to handle sign out request
 # def logout_request(request):
